ffd8deeae35c1aa024ead81bfef88b903d9b9e6d
creator154 <cteator898@gmail.com>
2026-05-21 16:25:15 +0530

    Update batch.py

--- handlers/batch.py
+++ handlers/batch.py
@@ -1,10 +1,15 @@
 from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from main import app
 
 
-@app.on_message(filters.private & filters.text)
-async def login_handler(client, message):
-    text = message.text
+@app.on_callback_query(filters.regex("batches"))
+async def batch_menu(client, query):
+    buttons = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Yakeen NEET 2027", callback_data="batch_1")]
+    ])
 
-    if "eyJ" in text:
-        await message.reply_text("✅ Token Saved Successfully")
+    await query.message.edit_text(
+        "📚 Select Batch",
+        reply_markup=buttons
+    )
