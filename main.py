8550f7af331ffb432c55d8cbd701ed20dc315c98
creator154 <cteator898@gmail.com>
2026-05-21 19:20:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,11 @@ from pyrogram import Client
 from config import *
 
 app = Client(
-    "course-bot",
+    "pw-bot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
-
-import handlers.start
-import handlers.login
-import handlers.batch
-import handlers.extractor
-import handlers.uploader
-import handlers.live
-import handlers.admin
 
 print("Bot Started")
 
